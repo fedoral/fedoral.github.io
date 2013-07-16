c767557594d0255a4e869ac3e3b533a124e37aec
root <root@ubuntu.(none)>
2013-07-16 12:48:14 +0800

    Site updated: Tue Jul 16 2013 12:48:14 GMT+0800 (CST)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-(function($){
+// main
+;(function($) {
+  var $about = $('header .about'),
+      $window = $(window);
+
+  var canToggle = true;
+  $(function() {
+    $('#toggle').on('click', function() {
+      canToggle = false;
+      $about.slideToggle('1000', 'linear', function() {});
+    });
+
+    $window.on('resize', toggleAbout);
+  });
+
+  var toggleAbout = function() {
+    if(canToggle) {
+      if($(window).width() >= 920) {
+        $about.show();
+      } else {
+        $about.hide();
+      }
+    }
+  };
+
+  toggleAbout();
+})(jQuery);
+
+// Gallery
+;(function($) {
   // Caption
   $('.entry').each(function(i){
     $(this).find('img').each(function(){
@@ -23,7 +52,6 @@
 
       callback();
       this.animate({opacity: 1}, 500);
-      parent.animate({height: width * nHeight / nWidth}, 500);
     });
   };
 
